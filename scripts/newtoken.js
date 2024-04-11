095b0c9205ae66d5312dc7aeca780cd7ba9e3937
ItsKandar <nmoreau15@myges.fr>
2024-04-11 15:18:32 +0200

    test api

--- scripts/newtoken.js
+++ scripts/newtoken.js
@@ -2,27 +2,25 @@ const config = JSON.parse(fs.readFileSync('conf.json'));
 const clientSecret = config.clientId;
 const clientId = config.clientSecret;
 
+// api URL https://api.spotify.com/v1/
 function newToken() {
     const clientId = '';
     const clientSecret = '';
     // Utilisation de btoa pour encoder en base64
     const token = btoa(`${clientId}:${clientSecret}`);
 
-    axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        params: {
-            grant_type: 'client_credentials',
-        },
+    const headers
+    const body = {
+        grant_type: 'client_credentials'
+    }
+
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
         headers: {
-            Authorization: `Basic ${token}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${token}`
         },
+        body: JSON.stringify(body)
     })
-    .then((response) => {
-        // Ici, mettez à jour l'élément HTML avec le token
-        document.getElementById('token').innerText = `Token: ${response.data.access_token}`;
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    .then(response => response.json())
 }
