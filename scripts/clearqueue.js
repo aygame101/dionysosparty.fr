635e243b73f1170e5f2c006481055213ae99cefe
aygame101 <aygame101@gmail.com>
2024-04-11 10:40:00 +0200

    main correction

--- scripts/clearqueue.js
+++
@@ -1,8 +0,0 @@
-// Script pour vider la file d'attente de musiques
-
-function clearQueue() {
-    // On vide la file d'attente
-    queue = [];
-    // On envoie un message de confirmation
-    message.channel.send("La file d'attente a été vidée.");
-}
